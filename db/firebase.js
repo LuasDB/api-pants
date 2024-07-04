94c5e03e9c4235bb0bf02d6b5a25b83f4ae2a416
Saul De la fuente <saul.delafuenteb@gmail.com>
2024-07-04 15:12:04 -0600

    Se modifica la forma de acceder a las credenciales de Firebase

--- db/firebase.js
+++ db/firebase.js
@@ -3,14 +3,24 @@ require('dotenv').config();
 //Se envía desde aqui el servidor
 const server = process.env.URL_SERVER
 
-//otro mode de usar firebase
-const admin = require('firebase-admin');
-const firebaseConfig = require('./firebaseConf');
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./../credentials.json");
 
 admin.initializeApp({
-    credential: admin.credential.cert(firebaseConfig),
-    databaseURL: `https://${process.env.PROJECT_ID}.firebaseio.com`
+  credential: admin.credential.cert(serviceAccount)
 });
+
+
+
+// //otro mode de usar firebase
+// const admin = require('firebase-admin');
+// const firebaseConfig = require('./firebaseConf');
+
+// admin.initializeApp({
+//     credential: admin.credential.cert(firebaseConfig),
+//     databaseURL: `https://${process.env.PROJECT_ID}.firebaseio.com`
+// });
 
 const db = admin.firestore();
 module.exports = {db,server}
